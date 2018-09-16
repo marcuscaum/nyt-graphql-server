6620174aa2c5b0b143fe63c9c985f991a9d0d744
marcuscaum <marcuslucas.jb@gmail.com>
2018-09-15 22:05:29 -0300

    Add config to the schema.js

--- src/schema.js
+++ src/schema.js
@@ -8,7 +8,42 @@ const { API_KEY } = process.env;
 
 const ArticleType = new GraphQLObjectType({
   name: 'Article',
-  fields: () => ({}),
+  fields: () => ({
+    id: globalIdField('Article'),
+    _id: {
+      type: GraphQLInt,
+      resolve: { id } => id,
+    },
+    url: {
+      type: GraphQLString,
+      resolve: { url } => url,
+    },
+    section: {
+      type: GraphQLString,
+      resolve: { section } => section,
+    },
+    title: {
+      type: GraphQLString,
+      resolve: { title } => title,
+    },
+    abstract: {
+      type: GraphQLString,
+      resolve: { abstract } => abstract,
+    },
+    publishedDate: {
+      type: GraphQLString,
+      resolve: { published_date } => published_date,
+    },
+    imageCaption: {
+      type: GraphQLString,
+      resolve: { media } => media[0].caption,
+    },
+    imageLarge: {
+      type: GraphQLString,
+      resolve: { media } => media[0]['media-metadata'][2].url,
+    },
+    totalShares: { type: GraphQLInt },
+  }),
 });
 
 const QueryType = new GraphQLObjectType({
