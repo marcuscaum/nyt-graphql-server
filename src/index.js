d63d6388ae45843dc521967727acf43119319f19
marcuscaum <marcuslucas.jb@gmail.com>
2018-09-15 21:35:26 -0300

    Add basic config

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// @flow
+
+import 'babel-polyfill';
+import Koa from 'koa';
+import graphqlHttp from 'koa-graphql';
+
+import { schema } from './schema';
+
+const PORT = process.env.PORT || 5000;
+const app = new Koa();
+
+app.use(
+  graphqlHttp({
+    schema,
+    graphiql: true,
+  }),
+);
+
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}`);
+});
---
+++ src/schema.js
@@ -0,0 +1,30 @@
+// @flow
+
+import fetch from 'node-fetch';
+import { globalIdField } from 'graphql-relay';
+import {
+  GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList,
+} from 'graphql';
+
+const { API_KEY } = process.env;
+
+const ArticleType = new GraphQLObjectType({
+  name: 'Article',
+  fields: () => ({}),
+});
+
+const QueryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: () => ({
+    allArticles: {
+      type: new GraphQLList(ArticleType),
+      resolve: async (_, { section, timePeriod }) => fetch(
+        `https://api.nytimes.com/svc/mostpopular/v2/mostshared/${section}/${timePeriod}.json?api-key=${API_KEY}`,
+      ),
+    },
+  }),
+});
+
+export default GraphQLSchema({
+  query: QueryType,
+});
